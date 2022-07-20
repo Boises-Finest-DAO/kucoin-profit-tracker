af7804a4a3d7853547bfae3adacfeea3d6b3d59a
BoiseITGuru <dev@boiseitguru.com>
2022-07-20 01:55:12 -0600

    track kucoin balances

--- main.go
+++ main.go
@@ -1,11 +1,6 @@
 package main
 
 import (
-	// "log"
-
-	// "github.com/boises-finest-dao/investmentdao-backend/internal/database"
-	// "github.com/boises-finest-dao/investmentdao-backend/internal/exchanges/kucoin"
-
 	"log"
 
 	"github.com/boises-finest-dao/investmentdao-backend/internal/database"
@@ -20,12 +15,6 @@ func main() {
 	// Initialize Database
 	database.Connect(AppConfig.GormConnection)
 	database.Migrate()
-
-	// kucoin.ConfigureConnection(AppConfig.KuCoinApiKey, AppConfig.KuCoinApiSecret, AppConfig.KuCoinApiPass)
-
-	// kucoin.GetTradingBalances()
-
-	// bot := helpers.GetBotDetails("c42a00d704fda9a62c54e15012f0dd0a994b4ab3c90c9185aa99e80edc931fd5")
 
 	tradingBalance := models.TradingBalance{
 		FundID: 1,
@@ -50,8 +39,6 @@ func main() {
 	database.Instance.Save(&tradingBalance)
 
 	// bot := services.GetBotByID(1)
-
-	// log.Println(balance)
 
 	// log.Println(services.EncryptString("123456789"))
 }
