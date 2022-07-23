6f3671e3e17eb1b72b6e7132cd148d710c68288b
BoiseITGuru <dev@boiseitguru.com>
2022-07-23 13:41:00 -0600

    more poc

--- main.go
+++ main.go
@@ -14,11 +14,22 @@ import (
 	"github.com/boises-finest-dao/investmentdao-backend/internal/services"
 	"github.com/gin-gonic/gin"
 	ginserver "github.com/go-oauth2/gin-server"
+	"github.com/go-oauth2/oauth2/v4/errors"
+	"github.com/go-oauth2/oauth2/v4/generates"
 	"github.com/go-oauth2/oauth2/v4/manage"
+	oauth2models "github.com/go-oauth2/oauth2/v4/models"
 	"github.com/go-oauth2/oauth2/v4/server"
 	"github.com/go-oauth2/oauth2/v4/store"
 	"github.com/procyon-projects/chrono"
 	"gorm.io/gorm/clause"
+)
+
+var (
+	dumpvar   bool
+	idvar     string
+	secretvar string
+	domainvar string
+	portvar   int
 )
 
 func main() {
@@ -40,16 +51,39 @@ func main() {
 
 func initRouter() *gin.Engine {
 	manager := manage.NewDefaultManager()
+	manager.SetAuthorizeCodeTokenCfg(manage.DefaultAuthorizeCodeTokenCfg)
 
 	// token store
-	manager.MustTokenStorage(store.NewFileTokenStore("data.db"))
+	manager.MustTokenStorage(store.NewMemoryTokenStore())
 
-	// JWT Auth
-	manager.MapAccessGenerate()
+	// generate jwt access token
+	// manager.MapAccessGenerate(generates.NewJWTAccessGenerate("", []byte("00000000"), jwt.SigningMethodHS512))
+	manager.MapAccessGenerate(generates.NewAccessGenerate())
+
+	clientStore := store.NewClientStore()
+	clientStore.Set(idvar, &oauth2models.Client{
+		ID:     idvar,
+		Secret: secretvar,
+		Domain: domainvar,
+	})
+	manager.MapClientStorage(clientStore)
 
 	// Initialize the oauth2 service
 	ginserver.InitServer(manager)
-	ginserver.SetPasswordAuthorizationHandler()
+	ginserver.SetPasswordAuthorizationHandler(func(ctx context.Context, clientID, username, password string) (userID string, err error) {
+		if username == "test" && password == "test" {
+			userID = "test"
+		}
+		return
+	})
+	ginserver.SetUserAuthorizationHandler(userAuthorizeHandler)
+	ginserver.SetInternalErrorHandler(func(err error) (re *errors.Response) {
+		log.Println("Internal Error:", err.Error())
+		return
+	})
+	ginserver.SetResponseErrorHandler(func(re *errors.Response) {
+		log.Println("Response Error:", re.Error.Error())
+	})
 	ginserver.SetAllowGetAccessRequest(true)
 	ginserver.SetClientInfoHandler(server.ClientFormHandler)
 
